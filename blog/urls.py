0b572e90c894e73387de576cd21c6b11da7ef16f
YasinAzadpour <yasinaazadpour@gmail.com>
2022-03-31 18:35:16 +0430

    home app added

--- blog/urls.py
+++ blog/urls.py
@@ -22,6 +22,7 @@ from django.urls.conf import include
 
 
 urlpatterns = [
+    path('/', include('accounts.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     *staticfiles_urlpatterns(),
