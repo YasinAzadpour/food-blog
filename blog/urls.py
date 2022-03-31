c12207257479837af5342b7408f5a6299ba9b1e3
YasinAzadpour <yasinaazadpour@gmail.com>
2022-03-31 18:16:56 +0430

    accounts app added

--- blog/urls.py
+++ blog/urls.py
@@ -18,10 +18,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
+from django.urls.conf import include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     *staticfiles_urlpatterns(),
     *static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT),
 ]
